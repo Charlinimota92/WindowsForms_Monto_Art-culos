f1cb47a9a3ee9711693b9cb6e7c87218bd2d5573
heurilinares <heurilinares@DESKTOP-7F9G3G4>
2024-02-12 12:32:31 -1200

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace WindowsForms_Monto_Artículos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Calcular = new System.Windows.Forms.Button();
+            this.btn_Limpiar = new System.Windows.Forms.Button();
+            this.btn_Salir = new System.Windows.Forms.Button();
+            this.txt_monto_total_de_artículos = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_descuento = new System.Windows.Forms.TextBox();
+            this.txt_itbis = new System.Windows.Forms.TextBox();
+            this.txt_monto_neto = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(52, 71);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Monto total de artículos";
+            // 
+            // btn_Calcular
+            // 
+            this.btn_Calcular.Font = new System.Drawing.Font("Microsoft JhengHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Calcular.Location = new System.Drawing.Point(56, 428);
+            this.btn_Calcular.Margin = new System.Windows.Forms.Padding(5);
+            this.btn_Calcular.Name = "btn_Calcular";
+            this.btn_Calcular.Size = new System.Drawing.Size(139, 64);
+            this.btn_Calcular.TabIndex = 1;
+            this.btn_Calcular.Text = "Calcular";
+            this.btn_Calcular.UseVisualStyleBackColor = true;
+            this.btn_Calcular.Click += new System.EventHandler(this.btn_Calcular_Click);
+            // 
+            // btn_Limpiar
+            // 
+            this.btn_Limpiar.Font = new System.Drawing.Font("Microsoft JhengHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Limpiar.Location = new System.Drawing.Point(312, 428);
+            this.btn_Limpiar.Margin = new System.Windows.Forms.Padding(5);
+            this.btn_Limpiar.Name = "btn_Limpiar";
+            this.btn_Limpiar.Size = new System.Drawing.Size(142, 64);
+            this.btn_Limpiar.TabIndex = 2;
+            this.btn_Limpiar.Text = "Limpiar";
+            this.btn_Limpiar.UseVisualStyleBackColor = true;
+            this.btn_Limpiar.Click += new System.EventHandler(this.btn_Limpiar_Click);
+            // 
+            // btn_Salir
+            // 
+            this.btn_Salir.Font = new System.Drawing.Font("Microsoft JhengHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Salir.Location = new System.Drawing.Point(459, 14);
+            this.btn_Salir.Margin = new System.Windows.Forms.Padding(5);
+            this.btn_Salir.Name = "btn_Salir";
+            this.btn_Salir.Size = new System.Drawing.Size(107, 47);
+            this.btn_Salir.TabIndex = 3;
+            this.btn_Salir.Text = "Salir";
+            this.btn_Salir.UseVisualStyleBackColor = true;
+            this.btn_Salir.Click += new System.EventHandler(this.btn_Salir_Click);
+            // 
+            // txt_monto_total_de_artículos
+            // 
+            this.txt_monto_total_de_artículos.Location = new System.Drawing.Point(275, 71);
+            this.txt_monto_total_de_artículos.Margin = new System.Windows.Forms.Padding(5);
+            this.txt_monto_total_de_artículos.Name = "txt_monto_total_de_artículos";
+            this.txt_monto_total_de_artículos.Size = new System.Drawing.Size(179, 29);
+            this.txt_monto_total_de_artículos.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(52, 134);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Descuento";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(52, 234);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 21);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Itbis";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(40, 325);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 21);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Monto Neto";
+            // 
+            // txt_descuento
+            // 
+            this.txt_descuento.Location = new System.Drawing.Point(198, 134);
+            this.txt_descuento.Name = "txt_descuento";
+            this.txt_descuento.Size = new System.Drawing.Size(184, 29);
+            this.txt_descuento.TabIndex = 8;
+            // 
+            // txt_itbis
+            // 
+            this.txt_itbis.Location = new System.Drawing.Point(198, 234);
+            this.txt_itbis.Name = "txt_itbis";
+            this.txt_itbis.Size = new System.Drawing.Size(184, 29);
+            this.txt_itbis.TabIndex = 9;
+            // 
+            // txt_monto_neto
+            // 
+            this.txt_monto_neto.Location = new System.Drawing.Point(198, 325);
+            this.txt_monto_neto.Name = "txt_monto_neto";
+            this.txt_monto_neto.Size = new System.Drawing.Size(184, 29);
+            this.txt_monto_neto.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(580, 506);
+            this.Controls.Add(this.txt_monto_neto);
+            this.Controls.Add(this.txt_itbis);
+            this.Controls.Add(this.txt_descuento);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_monto_total_de_artículos);
+            this.Controls.Add(this.btn_Salir);
+            this.Controls.Add(this.btn_Limpiar);
+            this.Controls.Add(this.btn_Calcular);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Calcular;
+        private System.Windows.Forms.Button btn_Limpiar;
+        private System.Windows.Forms.Button btn_Salir;
+        private System.Windows.Forms.TextBox txt_monto_total_de_artículos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_descuento;
+        private System.Windows.Forms.TextBox txt_itbis;
+        private System.Windows.Forms.TextBox txt_monto_neto;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_Monto_Artículos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Salir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_Limpiar_Click(object sender, EventArgs e)
+        {
+            txt_monto_total_de_artículos.Clear();
+            txt_descuento.Clear();
+            txt_monto_neto.Clear();
+            txt_itbis.Clear();
+        }
+
+        private void btn_Calcular_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txt_monto_total_de_artículos.Text, out double montoTotal))
+            {
+                double descuento = 0;
+                double itbis = montoTotal * 0.18; 
+
+                if (montoTotal >= 5001 && montoTotal <= 10000)
+                {
+                    descuento = montoTotal * 0.03;
+                }
+                else if (montoTotal > 10000 && montoTotal <= 15000)
+                {
+                    descuento = montoTotal * 0.05;
+                }
+                else if (montoTotal > 15000 && montoTotal <= 20000)
+                {
+                    descuento = montoTotal * 0.08;
+                }
+                else if (montoTotal > 20000)
+                {
+                    descuento = montoTotal * 0.1;
+                }
+                else
+                {
+                    MessageBox.Show("No aplica descuento.");
+                    return;
+                }
+
+                double montoNeto = montoTotal - descuento + itbis;
+
+                
+                txt_descuento.Text = descuento.ToString("C");
+                txt_itbis.Text = itbis.ToString("C");
+                txt_monto_neto.Text = montoNeto.ToString("C");
+            }
+            else
+            {
+                MessageBox.Show("Por favor ingrese un monto válido.", "Error de entrada");
+            }
+        }
+    }
+}
+
+    
